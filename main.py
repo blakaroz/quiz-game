df61895a13e6398cd285491479206883c07f9669
blakaroz <144008100+blakaroz@users.noreply.github.com>
2023-12-03 19:33:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -13,31 +13,3 @@ quiz = QuizBrain(question_bank)
 while quiz.still_has_question():
     quiz.next_question()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#
-# question_bank = []
-# for item in question_data:
-#     question = Question(item["text"], item["answer"])
-#     question_bank.append(question)
