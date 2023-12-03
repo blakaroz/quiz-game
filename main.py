091d0a7189ef352085411425d032eff97eafecf0
blakaro <144008100+blakaroz@users.noreply.github.com>
2023-12-03 19:32:52 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,43 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+question_bank = []
+for question in question_data:
+    q_text = question["text"]
+    q_answer = question["answer"]
+    new_question = Question(q_text, q_answer )
+    question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
+while quiz.still_has_question():
+    quiz.next_question()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
+# question_bank = []
+# for item in question_data:
+#     question = Question(item["text"], item["answer"])
+#     question_bank.append(question)
